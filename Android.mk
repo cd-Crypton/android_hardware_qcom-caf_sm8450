a6ed68fe330b99607a2c4327aae80f119eddff9c
Carlo Dee <carlodee.official@proton.me>
2023-04-07 17:46:39 +0800

    Changed conditional.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter $(LOCAL_PATH),$(PRODUCT_SOONG_NAMESPACES)),)
-ifneq ($(BUILD_WITHOUT_VENDOR), true)
+ifeq ($(TARGET_BOARD_PLATFORM),taro)
 include $(call first-makefiles-under,$(LOCAL_PATH))
-endif
 endif
